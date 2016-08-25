aee6f50585570cd242c9f75902ea8648ae6bcbca
Kevin Quillen <kevinquillen@gmail.com>
2016-08-25 13:34:43 -0400

    Implement the form ajax action

--- src/Form/PhantomJSCaptureTestForm.php
+++ src/Form/PhantomJSCaptureTestForm.php
@@ -30,7 +30,7 @@ class PhantomJSCaptureTestForm extends FormBase {
     $form['url'] = array(
       '#type' => 'textfield',
       '#title' => t('URL'),
-      '#description' => t('Absolute URL to the homepage that should be capture (it has to be a complete URL with http://).'),
+      '#description' => t('Absolute URL to the page that you want to capture (it must to be a complete URL with http://).'),
       '#default_value' => 'http://www.google.com',
     );
 
@@ -45,19 +45,19 @@ class PhantomJSCaptureTestForm extends FormBase {
     );
 
     $form['result'] = array(
-      '#prefix' => '<div id="phantomjs-capture-test-result">',
+      '#prefix' => '<div id="capture-result">',
       '#suffix' => '</div>',
       '#markup' => '',
     );
 
     $form['submit'] = array(
-      '#type' => 'submit',
+      '#type' => 'button',
       '#value' => t('Capture'),
-      "#ajax" => array(
-        "callback" => "phantomjs_capture_test_submit",
-        "wrapper" => "phantomjs-capture-test-result",
-        "method" => 'replace',
-        "effect" => "fade",
+      '#ajax' => array(
+        'callback' => array($this, 'capture'),
+        'wrapper' => 'capture-result',
+        'method' => 'replace',
+        'effect' => 'fade',
       ),
     );
 
@@ -72,33 +72,34 @@ class PhantomJSCaptureTestForm extends FormBase {
 
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
+  public function capture(array &$form, FormStateInterface $form_state) {
     $config = $this->config('phantomjs_capture.settings');
     $values = $form_state->getValues();
 
     // Build urls and destination.
     $url = $values['url'];
-    $file = 'test' . $values['format'];
-    $dest = \Drupal::config('system.file')->get('default_scheme') . '://' . $config->get('destination');
+    $file = 'capture_test' . $values['format'];
+    $destination = \Drupal::config('system.file')->get('default_scheme') . '://' . $config->get('destination');
 
     // Get the screen shot and create success/error message.
-    $output = '<div class="messages status"><ul><li>' . t('File have been generated. You can get it !url', array('!url' => l(t('here'), file_create_url($dest . '/' . $file)))) . '.</ul></li></div>';
-    if (!phantomjs_capture_screen($url, $dest, $file)) {
-      $output = '<div class="messages error"><ul><li>' . t('The address entered could not be retrieved.') . '</ul></li></div>';
+    $output = '<div class="messages status"><ul><li>' . $this->t('The file has been generated. You can view it <a href=":url">here</a>', array(':url' => file_create_url($destination . '/' . $file))) . '.</ul></li></div>';
+
+    if (!phantomjs_capture_screen($url, $file)) {
+      $output = '<div class="messages error"><ul><li>' . $this->t('The address entered could not be retrieved, or phantomjs could not perform the action.') . '</ul></li></div>';
     }
 
-    // Return.
     return array(
       'phantomjs_capture_test' => array(
         'result' => array(
-          '#prefix' => '<div id="phantomjs-capture-test-result">',
+          '#prefix' => '<div id="capture-result">',
           '#suffix' => '</div>',
           '#markup' => $output,
         ),
       ),
     );
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // empty
   }
 }
