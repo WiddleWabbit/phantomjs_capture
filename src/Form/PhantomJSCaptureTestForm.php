d928730a7d0aca5e5a26f5d0bec111f96a768bec
Kevin Quillen <kevinquillen@gmail.com>
2016-08-25 09:52:37 -0400

    Start bringing in old testing form code

--- src/Form/PhantomJSCaptureTestForm.php
+++ src/Form/PhantomJSCaptureTestForm.php
@@ -27,8 +27,39 @@ class PhantomJSCaptureTestForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('phantomjs_capture.settings');
+    $form['url'] = array(
+      '#type' => 'textfield',
+      '#title' => t('URL'),
+      '#description' => t('Absolute URL to the homepage that should be capture (it has to be a complete URL with http://).'),
+      '#default_value' => 'http://www.google.com',
+    );
 
+    $form['format'] = array(
+      '#type' => 'select',
+      '#title' => 'File format',
+      '#options' => array(
+        '.png' => 'png',
+        '.jpg' => 'jpg',
+        '.pdf' => 'pdf',
+      ),
+    );
+
+    $form['result'] = array(
+      '#prefix' => '<div id="phantomjs-capture-test-result">',
+      '#suffix' => '</div>',
+      '#markup' => '',
+    );
+
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Capture'),
+      "#ajax" => array(
+        "callback" => "phantomjs_capture_test_submit",
+        "wrapper" => "phantomjs-capture-test-result",
+        "method" => 'replace',
+        "effect" => "fade",
+      ),
+    );
 
     return $form;
   }
@@ -45,8 +76,29 @@ class PhantomJSCaptureTestForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config = $this->config('phantomjs_capture.settings');
     $values = $form_state->getValues();
 
-    parent::submitForm($form, $form_state);
+    // Build urls and destination.
+    $url = $values['url'];
+    $file = 'test' . $values['format'];
+    $dest = \Drupal::config('system.file')->get('default_scheme') . '://' . $config->get('destination');
+
+    // Get the screen shot and create success/error message.
+    $output = '<div class="messages status"><ul><li>' . t('File have been generated. You can get it !url', array('!url' => l(t('here'), file_create_url($dest . '/' . $file)))) . '.</ul></li></div>';
+    if (!phantomjs_capture_screen($url, $dest, $file)) {
+      $output = '<div class="messages error"><ul><li>' . t('The address entered could not be retrieved.') . '</ul></li></div>';
+    }
+
+    // Return.
+    return array(
+      'phantomjs_capture_test' => array(
+        'result' => array(
+          '#prefix' => '<div id="phantomjs-capture-test-result">',
+          '#suffix' => '</div>',
+          '#markup' => $output,
+        ),
+      ),
+    );
   }
 }
