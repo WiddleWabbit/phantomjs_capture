99fa5441ac85b3013d4da9f358e021c6dc8e5977
Kevin Quillen <kevinquillen@gmail.com>
2016-08-26 15:25:53 -0400

    refactor to use the service

--- src/Controller/PhantomJSCaptureInfoController.php
+++ src/Controller/PhantomJSCaptureInfoController.php
@@ -3,28 +3,38 @@
 namespace Drupal\phantomjs_capture\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\phantomjs_capture\PhantomJSCaptureHelper;
 
 class PhantomJSCaptureInfoController extends ControllerBase {
+
+  /**
+   * @var PhantomJSCaptureHelper
+   */
+  private $captureHelper;
+
+  /**
+   * PhantomJSCaptureInfoController constructor.
+   * @param PhantomJSCaptureHelper $capture_helper
+   */
+  public function __construct(PhantomJSCaptureHelper $capture_helper) {
+    $this->captureHelper = $capture_helper;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('phantomjs_capture.helper'));
+  }
 
   /**
    * Return basic information about the phantomjs binary, if detected.
    * @return array
    */
   public function index() {
-    $config = $this->config('phantomjs_capture.settings');
-
-    // @todo: the following code should be converted into a method of a service.
-
-    // If the binary is not given try the default path.
-    if (is_null($config->get('binary') || !file_exists($config->get('binary')))) {
-      return ['#markup' => 'PhantomJS binary was not found. Please install PhantomJS on the system.'];
-    }
-
-    // Execute PhantomJS to get its version, if PhantomJS was found.
-    $output = array();
-    exec($config->get('binary') . ' -v', $output);
-
-    return ['#markup' => $this->t('PhantomJS binary detected, version is :version', [':version' => $output[0]])];
+    $version = $this->captureHelper->getVersion();
+    return ['#markup' => $this->t('PhantomJS binary detected, version is :version', [':version' => $version])];
   }
 
 }
